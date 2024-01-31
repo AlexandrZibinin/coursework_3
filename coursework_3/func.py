f27d9f8c88a9fcab3c43f94e42751368b42e2f40
Alexandr Zibinin <zibinin.chel@gmail.com>
2024-02-01 02:59:32 +0300

    добавлена функция get_executed для получения только совершенных платежей

--- coursework_3/func.py
+++ coursework_3/func.py
@@ -5,8 +5,20 @@ operations = os.path.abspath('../operations.json')
 
 
 def get_dict_from_json(filename):
-    """получаем данные из файла по пути filename в виде списка словарей"""
+    """получаем данные из файла operation.json в виде списка словарей"""
     with open(filename, 'r') as f:
         data = f.read()
         return json.loads(data)
 
+
+def get_executed(all_operations):
+    """функция создает новый список словарей, включающих только перации типа EXECUTED"""
+    for data in all_operations:
+        if 'state' in data and data['state'] == 'EXECUTED':
+            print(data)
+
+
+#
+# all_operations = get_dict_from_json(operations)
+#
+# get_executed(all_operations)
